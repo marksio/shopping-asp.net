9f5a3234b511cc27e6dfe6e2b9516f225a2a4177
marksio <15073448@imail.sunway.edu.my>
2018-02-06 16:59:58 +0800

    Add cart

--- shopping/Models/shop.cs
+++ shopping/Models/shop.cs
@@ -10,12 +10,13 @@ namespace shopping.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
+        public string Desc { get; set; }
+        public string Pic { get; set; }
+        public string Link { get; set; }
         public decimal Price { get; set; }
     }
     public class shopDBContext : DbContext
     {
-        public DbSet<shop> Movies { get; set; }
+        public DbSet<shop> shop { get; set; }
     }
 }
