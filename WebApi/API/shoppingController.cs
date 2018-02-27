682fb3861f845a21af05a2c18433b8b5f7580a07
marksio <15073448@imail.sunway.edu.my>
2018-02-27 11:20:17 +0800

    Created API file that allow produce Json format

---
+++ WebApi/API/shoppingController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+//using Newtonsoft.Json;
+
+namespace WebApi.API
+{
+    public class shoppingController : ApiController
+    {
+        SchoolMangementEntities _ctx = null; // SchoolMangementEntities is data model name
+        public shoppingController()
+        {
+            _ctx = new SchoolMangementEntities();
+        }
+        public HttpResponseMessage GetStudents()
+        {
+            // 
+            List<tbl_Student> students = null;
+            //System.Web.Mvc.JsonResult jsonResult = new System.Web.Mvc.JsonResult();
+            
+            try
+            {
+                students = _ctx.tbl_Student.ToList();
+                //jsonResult.Data = JsonObjectToString(students);
+                Request.Headers.Add("Accept", "text/json");
+                
+            }
+            catch
+            {
+
+                students = null;
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, students);
+            //return jsonResult;
+        }
+
+        //public static String JsonObjectToString(Object jsonObject)
+        //{
+        //    JsonSerializerSettings theJsonSerializerSettings = new JsonSerializerSettings();
+
+        //    theJsonSerializerSettings.TypeNameHandling = TypeNameHandling.None;
+        //    theJsonSerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+        //    theJsonSerializerSettings.MissingMemberHandling = MissingMemberHandling.Ignore;
+
+        //    string jsonString = JsonConvert.SerializeObject(jsonObject, theJsonSerializerSettings);
+
+        //    return jsonString;
+        //}
+    }
+}
--- shopping/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace shopping.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
